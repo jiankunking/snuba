69fee6253c6a78e7c2668bf6c86692e4df8fe012
jiankunking <jiankunking@163.com>
2020-02-26 20:02:32 +0800

    修改snuba api监听地址

--- snuba/cli/api.py
+++ snuba/cli/api.py
@@ -1,19 +1,20 @@
-from typing import Optional
-
-import click
-
-from snuba.environment import setup_logging
-
-
-@click.command()
-@click.option("--debug", is_flag=True)
-@click.option("--log-level", help="Logging level to use.")
-def api(*, debug: bool, log_level: Optional[str] = None) -> None:
-    from snuba import settings
-    from snuba.web.views import application
-    from werkzeug.serving import WSGIRequestHandler
-
-    setup_logging(log_level)
-
-    WSGIRequestHandler.protocol_version = "HTTP/1.1"
-    application.run(port=settings.PORT, threaded=True, debug=debug)
+ from typing import Optional
+ 
+ import click
+ 
+ from snuba.environment import setup_logging
+ 
+ 
+ @click.command()
+ @click.option("--debug", is_flag=True)
+ @click.option("--log-level", help="Logging level to use.")
+ def api(*, debug: bool, log_level: Optional[str] = None) -> None:
+     from snuba import settings
+     from snuba.web.views import application
+     from werkzeug.serving import WSGIRequestHandler
+ 
+     setup_logging(log_level)
+ 
+     WSGIRequestHandler.protocol_version = "HTTP/1.1"
+     host = "0.0.0.0"
+     application.run(host=host, port=settings.PORT, threaded=True, debug=debug)
